6f632862e2b445333dab5650c12a9f88b0305a5f
Qiwei He <qiweih1@uci.edu>
2021-01-16 16:07:19 +0800

    Initial commit - Qiwei He
    created main function and included some basic libraries

--- src/sh.c
+++ src/sh.c
@@ -1,1 +1,16 @@
-a
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+int main(){
+    char input[80], *buffer, full[80];
+    while (1)
+    {
+        fgets(input, (sizeof input / sizeof input[0]), stdin);
+        if(strcmp(input, "quit")){break;}
+    }
+    
+   
+    return 0;
+}
