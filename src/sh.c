d64b95655d6d639f35af81e25145a5ab0f018747
Liwei Lu <liweil2@circinus-22.ics.uci.edu>
2021-01-26 00:54:50 -0800

    add sh.h into sh.c

--- src/sh.c
+++ src/sh.c
@@ -36,6 +36,34 @@
 #define MAX_LINE 100
 #define SH_TOK_DELIM " \t\r\n\a"
 
+struct Job{
+	pid_t pid;
+	char command_line[80];
+	int status; // 0 for empty, 1 for Running, 2 for Foreground, 3 for Stopped
+};
+
+
+
+int compare(int a, int b);
+char *sh_read_line(void);
+int execute(char **args);
+
+// build in command
+void print_jobs();
+void run_fg(char** args);
+void run_bg(char** args);
+void run_kill(char** args);
+
+// IO redirection
+void change_output(char *file_name);
+void change_input(char *file_name);
+
+// Signal handler
+void int_handler(int sig);  // terminate
+void stop_handler(int sig); // stop
+
+void child_handler(int sig);
+
 struct Job jobs[Maxjob];
 
 int compare(int a, int b){
